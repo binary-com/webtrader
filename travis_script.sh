8ce1c1c62366ffeeb301d8b8416a16ce6a22d152
Arnab Karmakar <arnab.k@gmail.com>
2015-04-10 22:02:39 -0600

    Fixed travis CI conf

--- travis_script.sh
+++ travis_script.sh
@@ -2,16 +2,12 @@
 
 release_prod="$(git log -1 --pretty=%B | awk '/[release_prod]]/ { print $1 } ')"
 release_beta="$(git log -1 --pretty=%B | awk '/[release_beta]]/ { print $1 } ')"
-if [ -n $release ]; then
-    git config --global user.email "arnab@binary.com"
-    git config --global user.name "Arnab Karmakar"
-    if [ -n $release_beta ]; then
-        mkdir beta
-        mv dist/compressed/* beta
-        mv beta dist/compressed
-        grunt gh-pages:gh-pages-beta
-    fi
-    if [ -n $release_prod ]; then
-      	grunt gh-pages:gh-pages-prod
-    fi
+if [ ! -z "$release_beta" -a "$release_beta" != " " ]; then
+    mkdir beta
+    mv dist/compressed/* beta
+    mv beta dist/compressed
+    grunt gh-pages:gh-pages-beta
+fi
+if [ ! -z "$release_prod" -a "$release_prod" != " " ]; then
+    grunt gh-pages:gh-pages-prod
 fi
