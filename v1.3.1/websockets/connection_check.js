93a3edc0d13044995765fd14ce671d2c8ed6f171
Arnab Karmakar <arnab@binary.com>
2015-09-28 22:44:10 +0000

    Commiting v1.3.1 using TravisCI and GruntJS build process for prod

---
+++ v1.3.1/websockets/connection_check.js
@@ -0,0 +1,1 @@
+define([],function(){var a=null;return{init:function(b,c){$(document).everyTime(15e3,null,function(){var d=(new Date).getTime()-(a||(new Date).getTime());{var e=d.chartIDs;if(b.send(JSON.stringify({forget:c[a].tickStreamingID})),c[a].tickStreamingID=null,e&&e.length>0){var f={ticks:e[0].instrumentCode,end:"latest",instrumentCdAndTp:a.toUpperCase()};
