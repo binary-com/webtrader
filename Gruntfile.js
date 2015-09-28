42a5466a2caf9391820ea2323906e05701744bd3
Arnab Karmakar <arnab@binary.com>
2015-09-28 16:36:24 -0600

    Updating Grunt file with new repo details

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function (grunt) {
                 options: {
                     base: 'dist/compressed',
                     add: true,
-                    repo: 'https://' + process.env.GIT_KEY + '@github.com/regentmarkets/highcharts.git',
+                    repo: 'https://' + process.env.GIT_KEY + '@github.com/binary-com/webtrader.git',
                     message: 'Commiting v<%=pkg.version%> using TravisCI and GruntJS build process for beta'
                 },
                 src: ['**/*']
@@ -126,7 +126,7 @@ module.exports = function (grunt) {
                 options: {
                     base: 'dist/compressed',
                     add: true,
-                    repo: 'https://' + process.env.GIT_KEY + '@github.com/regentmarkets/highcharts.git',
+                    repo: 'https://' + process.env.GIT_KEY + '@github.com/binary.com/webtrader.git',
                     message: 'Commiting v<%=pkg.version%> using TravisCI and GruntJS build process for prod'
                 },
                 src: ['**/*']
