bfb8a78ada7a4257f44ce7c18ea89ab7659a9d1b
Arnab Karmakar <arnab@binary.com>
2015-10-02 01:01:05 -0600

    https://trello.com/c/H81naQGE/6-change-the-deploy-process

--- travis_script.sh
+++ travis_script.sh
@@ -2,14 +2,13 @@
 
 git config --global user.email "arnab@binary.com"
 git config --global user.name "Arnab Karmakar"
-release_prod="$(git log -1 --pretty=%B | awk '/[release_prod]]/ { print $1 } ')"
-release_beta="$(git log -1 --pretty=%B | awk '/[release_beta]]/ { print $1 } ')"
-if [ ! -z "$release_beta" -a "$release_beta" != " " ]; then
+echo $TRAVIS_BRANCH
+if [[ $TRAVIS_BRANCH == 'development' ]]; then
     mkdir beta
     mv dist/compressed/* beta
     mv beta dist/compressed
-    grunt gh-pages:gh-pages-beta-uncompressed
+    grunt gh-pages:compressed
 fi
-if [ ! -z "$release_prod" -a "$release_prod" != " " ]; then
-    grunt gh-pages:gh-pages-prod-uncompressed
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    grunt gh-pages:compressed
 fi
