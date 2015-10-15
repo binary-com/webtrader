dcad2fca2b0ebd53f741222fcf8b1389f20a110f
Arnab Karmakar <arnab.k@gmail.com>
2015-10-15 16:05:24 -0600

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -4,7 +4,7 @@ git config --global user.email "arnab@binary.com"
 git config --global user.name "Arnab Karmakar"
 echo GIT Branch : $TRAVIS_BRANCH , Pull request number : $TRAVIS_PULL_REQUEST
 
-if [ $TRAVIS_BRANCH = "development" ]; then
+if [ $TRAVIS_BRANCH = development ]; then
     mkdir beta
     mv dist/compressed/* beta
     mv beta dist/compressed
