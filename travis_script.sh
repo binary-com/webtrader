8667c179ea5b2029c88bb7eb017e2c90fa37bc6b
Arnab Karmakar <arnab.k@gmail.com>
2015-10-15 15:20:09 -0600

    Testing equality check if travis-ci

--- travis_script.sh
+++ travis_script.sh
@@ -3,6 +3,7 @@
 git config --global user.email "arnab@binary.com"
 git config --global user.name "Arnab Karmakar"
 echo GIT Branch : $TRAVIS_BRANCH , Pull request number : $TRAVIS_PULL_REQUEST
+echo Testing equality [ $TRAVIS_BRANCH = 'development' ];
 
 #This if block is true when a PR is opened from development branch to master branch
 if [ $TRAVIS_BRANCH = 'development' ]; then
