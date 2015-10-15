0ecee468f944cd9faa1aa6ed00bb02b89d2d9ecf
Arnab Karmakar <arnab.k@gmail.com>
2015-10-15 15:55:10 -0600

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -3,14 +3,12 @@
 git config --global user.email "arnab@binary.com"
 git config --global user.name "Arnab Karmakar"
 echo GIT Branch : $TRAVIS_BRANCH , Pull request number : $TRAVIS_PULL_REQUEST
-echo Testing equality [ $TRAVIS_BRANCH = 'development' ]; 
 
-#This if block is true when a PR is opened from development branch to master branch
-if [ $TRAVIS_BRANCH = 'development' ]; then
+if [ $TRAVIS_BRANCH = "development" ]; then
     mkdir beta
     mv dist/compressed/* beta
     mv beta dist/compressed
 fi
-if [ $TRAVIS_BRANCH = 'master' ]; then
+if [ $TRAVIS_BRANCH = "master" ]; then
     grunt gh-pages:travis-deploy
 fi
