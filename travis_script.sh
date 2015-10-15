a96f1ffbf5a859e4edc3d8bcdf235667edae9403
Arnab Karmakar <arnab.k@gmail.com>
2015-10-15 16:17:01 -0600

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -4,11 +4,9 @@ git config --global user.email "arnab@binary.com"
 git config --global user.name "Arnab Karmakar"
 echo GIT Branch : $TRAVIS_BRANCH , Pull request number : $TRAVIS_PULL_REQUEST
 
-if [ $TRAVIS_BRANCH = development ]; then
     mkdir beta
     mv dist/compressed/* beta
     mv beta dist/compressed
-fi
 if [ $TRAVIS_BRANCH = "master" ]; then
     grunt gh-pages:travis-deploy
 fi
