b513e3a98bdf69001f7f56ecc618a776325453db
amin roosta <amin.roosta@outlook.com>
2015-11-18 12:19:06 +0330

    add contract-type selectmenu

--- src/trade/tradeDialog.js
+++ src/trade/tradeDialog.js
@@ -2,10 +2,7 @@
  * Created by amin on November 18, 2015.
  */
 
-define(['jquery','text!trade/tradeDialog.html'], function ($,$html) {
-    $html = $($html);
-
-
+define(['jquery', 'windows/windows', 'text!trade/tradeDialog.html', 'css!trade/tradeDialog.css', 'jquery-ui'], function ($, windows, $html) {
     /* The data for each symbol is in the following format:
       data = {
          symbol: "frxXAUUSD",
@@ -16,9 +13,26 @@ define(['jquery','text!trade/tradeDialog.html'], function ($,$html) {
          events: [{ dates: "Fridays", descrip: "Closes early (at 21:00)" }, { dates: "2015-11-26", descrip: "Closes early (at 18:00)" }],
          times: { open: ["00:00:00"], close: ["23:59:59"], settlement: "23:59:59" }
        } */
-    function init(data) {
-        console.warn($html[0]);
-        console.warn(data);
+    var data = null;
+    var dialog = null; // trade dialog
+    var contract_type = null;
+    $html = $($html);
+
+    function init(_data) {
+        console.warn($html);
+        data = _data;
+        dialog = windows.createBlankWindow($html, {
+            title: data.display_name,
+            //close: fn, // callback for dialog close event
+            //open: fn,  // callback for dialog open event
+            resizable:false,
+            collapsable:false,
+            minimizable: false,
+            maximizable: false,
+            height: 500
+        });
+        contract_type = $html.find('.contract-type').selectmenu({});
+        dialog.dialog('open');
     }
 
     return {
