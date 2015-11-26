96179094b1c43bd370f003cec8bfe55226e83b32
Arnab Karmakar <arnab@binary.com>
2015-11-26 12:11:07 -0700

    Completed -> https://trello.com/c/wIaAKBCh/191-add-indicator-balance-of-power-displays-undefined-in-the-chart https://trello.com/c/1boHejiR/173-add-dot-chart https://trello.com/c/Mu2iQg9X/172-add-line-dot-chart https://trello.com/c/NBRvx15E/187-highcharts-betav2-0-4-api-token-should-be-two-different-words-within-the-portfolio-pop-up https://trello.com/c/V0wsC8Jf/193-resolve-xss-vulnerabilities https://trello.com/c/v44glPaf/186-high-charts-beta-v2-0-4-minutes-text-within-the-charting-popup-is-with-different-font-styling-it-needs-to-be-with-standard-font https://trello.com/c/hA0NK97N/216-remove-indicators-is-not-working-in-beta

--- src/windows/windows.js
+++ src/windows/windows.js
@@ -357,7 +357,7 @@ define(['jquery', 'navigation/navigation', 'jquery.dialogextend', 'modernizr', '
                         /* return a random element of an array */
                         var rand = function (arr) { return arr[ Math.floor(Math.random()*arr.length) ]; };
                         var timePeriods = ['2h', '4h', '8h', '1d'];
-                        var chartTypes = ['candlestick', 'line', 'ohlc', 'spline'];
+                        var chartTypes = ['candlestick', 'line', 'ohlc', 'spline']; //This is not the complete chart type list, however its good enough to randomly select one from this subset
                         for (var inx = 0; inx < counts.total; ++inx){
                             var submarkets = rand(markets).submarkets;
                             var symbols = rand(submarkets).instruments;
