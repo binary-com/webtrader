a3e845ca3a69ff0a453eba3f0b6ebde22965af27
Arnab Karmakar <arnab@binary.com>
2015-12-07 23:53:22 -0700

    Added startTime, endTime, entrySpotTime, barrierPrice parameters Implemented Shooting start indicator

---
+++ src/charts/indicators/cdlshootingstar/cdlshootingstar.js
@@ -0,0 +1,77 @@
+/**
+ * Created by arnab on 12/06/15
+ */
+
+define(["jquery", "jquery-ui", 'color-picker'], function($) {
+
+    function closeDialog() {
+        $(this).dialog("close");
+        $(this).find("*").removeClass('ui-state-error');
+    }
+
+    function init( containerIDWithHash, _callback ) {
+
+        require(['text!charts/indicators/cdlshootingstar/cdlshootingstar.html'], function ( $html ) {
+
+            $html = $($html);
+            //$html.hide();
+            $html.appendTo("body");
+            //$html.find('select').selectmenu(); TODO for some reason, this does not work
+
+            $html.dialog({
+                autoOpen: false,
+                resizable: false,
+                width: 350,
+                modal: true,
+                my: 'left',
+                at: 'left',
+                of: window,
+                buttons: [
+                    {
+                        text: "Ok",
+                        click: function() {
+
+                            require(['charts/indicators/highcharts_custom/cdlshootingstar'], function ( cdlshootingstar ) {
+                                cdlshootingstar.init();
+                                //Add CDLSHOOTINGSTAR for the main series
+                                $($(".cdlshootingstar").data('refererChartID')).highcharts().series[0].addCDLSHOOTINGSTAR();
+                            });
+
+                            closeDialog.call($html);
+                        }
+                    },
+                    {
+                        text: "Cancel",
+                        click: function() {
+                            closeDialog.call(this);
+                        }
+                    }
+                ]
+            });
+
+            if ($.isFunction(_callback))
+            {
+                _callback( containerIDWithHash );
+            }
+
+        });
+
+    }
+
+    return {
+
+        open : function ( containerIDWithHash ) {
+
+            if ($(".cdlshootingstar").length == 0)
+            {
+                init( containerIDWithHash, this.open );
+                return;
+            }
+
+            $(".cdlshootingstar").data('refererChartID', containerIDWithHash).dialog( "open" );
+
+        }
+
+    };
+
+});
