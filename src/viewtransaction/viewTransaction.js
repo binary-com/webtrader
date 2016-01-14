c2ab78ea5a393423ebbc8a270c2bffd52ccda8f8
amin roosta <amin.roosta@outlook.com>
2016-01-14 16:19:43 +0330

    create viewTransaction file & folder

---
+++ src/viewtransaction/viewTransaction.js
@@ -0,0 +1,8 @@
+﻿/**
+ * Created by amin on January 14, 2016.
+ */
+
+define(["jquery", "windows/windows", "websockets/binary_websockets", "jquery-growl", 'common/util'], function($, windows, liveapi){
+
+  return { };
+});
