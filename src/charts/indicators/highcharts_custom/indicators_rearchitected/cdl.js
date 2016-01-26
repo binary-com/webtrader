a68821870798329b5f9a3cbe54b29e3d4f1f8ff9
Mahboobeh Mohammadi <mahboub.meng@gmail.com>
2016-01-26 13:29:00 +0330

    Convetred CDL3STARSSOUTH && CDL3WHITESOLDIERS CDLs

--- src/charts/indicators/highcharts_custom/indicators_rearchitected/cdl.js
+++ src/charts/indicators/highcharts_custom/indicators_rearchitected/cdl.js
@@ -290,26 +290,90 @@ CDL.prototype.CDL3OUTSIDE = function() {
     };
 };
 
+CDL.prototype.CDL3STARSSOUTH = function () {
+    var candleOne_Index = this.priceData.length - 1;
+    var candleTwo_Index = candleOne_Index - 1;
+    var candleThree_Index = candleOne_Index - 2;
+
+    var candleThree_Open = this.priceData[candleThree_Index].open,
+        candleThree_Close = this.priceData[candleThree_Index].close,
+        candleThree_Low = this.priceData[candleThree_Index].low,
+        candleThree_High = this.priceData[candleThree_Index].high;
+
+    var candleTwo_Open = this.priceData[candleTwo_Index].open,
+        candleTwo_Close = this.priceData[candleTwo_Index].close,
+        candleTwo_Low = this.priceData[candleTwo_Index].low,
+        candleTwo_High = this.priceData[candleTwo_Index].high;
+
+    var candleOne_Open = this.priceData[candleOne_Index].open,
+        candleOne_Close = this.priceData[candleOne_Index].close,
+        candleOne_Low = this.priceData[candleOne_Index].low,
+        candleOne_High = this.priceData[candleOne_Index].high;
+
+    var isCandleThree_Bullish = candleThree_Close > candleThree_Open,
+        isCandleThree_Bearish = candleThree_Close < candleThree_Open;
+    var isCandleTwo_Bullish = candleTwo_Close > candleTwo_Open,
+        isCandleTwo_Bearish = candleTwo_Close < candleTwo_Open;
+    var isCandleOne_Bullish = candleOne_Close > candleOne_Open,
+        isCandleOne_Bearish = candleOne_Close < candleOne_Open;
+
+    var candleThreeBodySize = Math.abs(candleThree_Close - candleThree_Open),
+        candleTwoBodySize = Math.abs(candleTwo_Close - candleTwo_Open),
+        candleOneBodySize = Math.abs(candleOne_Close - candleOne_Open);
+
+    var candleThreeLowerShadow = Math.abs(candleThree_Low - Math.min(candleThree_Close, candleThree_Open)),
+       candleThreeUpperShadow = Math.abs(candleThree_High - Math.max(candleThree_Close, candleThree_Open));
+
+    var isBullishContinuation = isCandleThree_Bearish
+                && (candleThreeLowerShadow >= (candleThreeBodySize * 2)) && (candleThreeUpperShadow < (candleThreeBodySize * 0.1)) //A black candlestick with almost no upper shadow and a long lower shadow appears on the first day.
+                && isCandleTwo_Bearish && (candleTwo_Low > candleThree_Low) && (candleTwo_Open < candleThree_Open) && (candleTwo_Close < candleThree_Close) && (candleTwoBodySize < candleThreeBodySize) // The next day is another black candlestick closing below the previous day’s close and having an opening in the range of the previous day’s body. However, it has a higher low.
+                && isCandleOne_Bearish && (candleOne_Low > candleTwo_Low) && (candleOne_Open === candleOne_High) && (candleOne_Low === candleOne_Close) && (candleOneBodySize < candleTwoBodySize);//The last day is a small black Marubozu with a higher low
+
+    //It's a bullish candlestick
+    var isBearishContinuation = false;
+
+    return {
+        isBear: isBullishContinuation,
+        isBull: isBearishContinuation
+    };
+};
+
+CDL.prototype.CDL3WHITESOLDIERS = function () {
+    var candleOne_Index = this.priceData.length - 1;
+    var candleTwo_Index = candleOne_Index - 1;
+    var candleThree_Index = candleOne_Index - 2;
+
+    var candleThree_Open = this.priceData[candleThree_Index].open,
+        candleThree_Close = this.priceData[candleThree_Index].close;
+    var candleTwo_Open = this.priceData[candleTwo_Index].open,
+        candleTwo_Close = this.priceData[candleTwo_Index].close;
+    var candleOne_Open = this.priceData[candleOne_Index].open,
+        candleOne_Close = this.priceData[candleOne_Index].close;
+
+    var isCandleThree_Bullish = candleThree_Close > candleThree_Open;
+    var isCandleTwo_Bullish = candleTwo_Close > candleTwo_Open;
+    var isCandleOne_Bullish = candleOne_Close > candleOne_Open;
+
+    var isBullishContinuation = isCandleThree_Bullish
+                                && isCandleTwo_Bullish && (candleTwo_Open > candleThree_Open && candleTwo_Open <= candleThree_Close && candleTwo_Close > candleThree_Close)
+                                && isCandleOne_Bullish && (candleOne_Open > candleTwo_Open && candleOne_Open <= candleTwo_Close && candleOne_Close > candleTwo_Close);
+
+    //It's a bullish candlestick
+    var isBearishContinuation = false;
+
+    return {
+        isBear: isBullishContinuation,
+        isBull: isBearishContinuation
+    };
+}
+
 CDL.prototype.calculateIndicatorValue = function(cdlPatternCode) {
     var ret;
     var time = this.priceData[this.priceData.length - 1].time;
     switch(cdlPatternCode.toUpperCase()) {
         case 'CDL2CROWS':
             var cdlObject = this.CDL2CROWS();
-            if (cdlObject.isBull) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : blue">TC</span>',
-                    text : 'Two crows : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : red">TC</span>',
-                    text : 'Two crows : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TC', 'Two crows');
             break;
         case 'CDLDOJI':
             var candleOne_Index         = this.priceData.length - 1;
@@ -318,89 +382,51 @@ CDL.prototype.calculateIndicatorValue = function(cdlPatternCode) {
                 candleOne_Low           = this.priceData[candleOne_Index].low,
                 candleOne_Close         = this.priceData[candleOne_Index].close;
             var cdlObject = this.CDLDOJI(candleOne_Open, candleOne_High, candleOne_Low, candleOne_Close);
-            if (cdlObject.isBull) {
-                ret = {
-                    x: time,
-                    title: '<span style="color : blue">D</span>',
-                    text: 'Doji : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x: time,
-                    title: '<span style="color : red">D</span>',
-                    text: 'Doji : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'D', 'Doji');
             break;
         case 'CDL3BLACKCROWS':
             var cdlObject = this.CDL3BLACKCROWS();
-            if (cdlObject.isBull) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : blue">TBC</span>',
-                    text : 'Three Black crows : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : red">TBC</span>',
-                    text : 'Three Black crows : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TBC', 'Three Black crows');
             break;
         case 'CDL3INSIDE':
             var cdlObject = this.CDL3INSIDE();
-            if (cdlObject.isBull) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : blue">TIUD</span>',
-                    text : 'Three Inside Up/Down : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : red">TIUD</span>',
-                    text : 'Three Inside Up/Down : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TIUD', 'Three Inside Up/Down');
             break;
         case 'CDL3LINESTRIKE':
             var cdlObject = this.CDL3LINESTRIKE();
-            if (cdlObject.isBull) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : blue">TLS</span>',
-                    text : 'Three-Line Strike : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : red">TLS</span>',
-                    text : 'Three-Line Strike : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TLS', 'Three-Line Strike');
             break;
         case 'CDL3OUTSIDE':
             var cdlObject = this.CDL3OUTSIDE();
-            if (cdlObject.isBull) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : blue">TOUD</span>',
-                    text : 'Three Outside Up/Down : Bull'
-                };
-            }
-            else if (cdlObject.isBear) {
-                ret = {
-                    x : time,
-                    title : '<span style="color : red">TOUD</span>',
-                    text : 'Three Outside Up/Down : Bear'
-                };
-            }
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TOUD', 'Three Outside Up/Down');
             break;
+        case 'CDL3STARSSOUTH':
+            var cdlObject = this.CDL3STARSSOUTH();
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TSS', 'Three Stars In The South');
+            break;
+        case 'CDL3WHITESOLDIERS':
+            var cdlObject = this.CDL3WHITESOLDIERS();
+            ret = CDLADDFLAGINFO(cdlObject, time, 'TWS', 'Three Advancing White Soldiers');
+            break;
+    }
+    return ret;
+};
+
+CDLADDFLAGINFO = function (cdlObject, time, shortDisplayName, longDisplayName) {
+    var ret;
+    if (cdlObject.isBull) {
+        ret = {
+            x: time,
+            title: '<span style="color : blue">' + shortDisplayName + '</span>',
+            text: longDisplayName + ' : Bull'
+        };
+    }
+    else if (cdlObject.isBear) {
+        ret = {
+            x: time,
+            title: '<span style="color : red">' + shortDisplayName + '</span>',
+            text: longDisplayName + ' : Bear'
+        };
     }
     return ret;
 };
