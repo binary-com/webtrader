2e13757f5d20a0d68cca774a3f0605f9cbb36a26
Mahboobeh Mohammadi <mahboub.meng@gmail.com>
2016-02-05 23:39:24 +0330

    Add Coppock curve indicator

---
+++ src/charts/indicators/cc/cc_level.js
@@ -0,0 +1,123 @@
+﻿/**
+ * Created by Mahboob>M on 2/5/16.
+ */
+
+define(["jquery", "jquery-ui", 'color-picker', 'ddslick'], function ($) {
+
+    var callBackAfterOKPressed = undefined;
+    function closeDialog() {
+        $(this).dialog("close");
+        $(this).find("*").removeClass('ui-state-error');
+    }
+
+    function init( containerIDWithHash, _callback ) {
+
+        var Level = function (level, stroke, strokeWidth, dashStyle) {
+            this.level = level;
+            this.stroke = stroke;
+            this.strokeWidth = strokeWidth;
+            this.dashStyle = dashStyle;
+        };
+
+        require(['text!charts/indicators/cc/cc_level.html'], function ( $html ) {
+
+            var defaultStrokeColor = '#cd0a0a';
+
+            $html = $($html);
+            $html.appendTo("body");
+            $html.find("input[type='button']").button();
+
+            $html.find("#cc_level_stroke").colorpicker({
+                part:	{
+                    map:		{ size: 128 },
+                    bar:		{ size: 128 }
+                },
+                select:			function(event, color) {
+                    $("#cc_level_stroke").css({
+                        background: '#' + color.formatted
+                    }).val('');
+                    defaultStrokeColor = '#' + color.formatted;
+                },
+                ok:             			function(event, color) {
+                    $("#cc_level_stroke").css({
+                        background: '#' + color.formatted
+                    }).val('');
+                    defaultStrokeColor = '#' + color.formatted;
+                }
+            });
+
+            var selectedDashStyle = "Solid";
+            $('#cc_level_dashStyle').ddslick({
+                imagePosition: "left",
+                width: 118,
+                background: "white",
+                onSelected: function (data) {
+                    $('#cc_level_dashStyle .dd-selected-image').css('max-width', '85px');
+                    selectedDashStyle = data.selectedData.value
+                }
+            });
+            $('#cc_level_dashStyle .dd-option-image').css('max-width', '85px');
+
+
+            $html.dialog({
+                autoOpen: false,
+                resizable: false,
+                width: 280,
+                modal: true,
+                my: 'center',
+                at: 'center',
+                of: window,
+                dialogClass: 'cc-ui-dialog',
+                buttons: [
+                    {
+                        text: "OK",
+                        click: function() {
+
+                            if (callBackAfterOKPressed) {
+                                callBackAfterOKPressed([new Level(parseFloat($html.find(".cc_level_input_width_for_level").val()),
+                                    defaultStrokeColor, parseInt($html.find("#cc_level_strokeWidth").val()),
+                                    selectedDashStyle)]);
+                            }
+
+                            closeDialog.call($html);
+                        }
+                    },
+                    {
+                        text: "Cancel",
+                        click: function() {
+                            closeDialog.call(this);
+                        }
+                    }
+                ]
+            });
+            $html.find('select').selectmenu({
+                width : 120
+            });
+
+            if ($.isFunction(_callback))
+            {
+                _callback( containerIDWithHash, callBackAfterOKPressed );
+            }
+
+        });
+
+    }
+
+    return {
+
+        open : function ( containerIDWithHash, _callback ) {
+
+            callBackAfterOKPressed = _callback;
+            if ($(".cc_level").length == 0)
+            {
+                init( containerIDWithHash, this.open );
+                return;
+            }
+
+            $(".cc_level").dialog( "open" );
+
+        }
+
+    };
+
+});
---
+++ src/charts/indicators/highcharts_custom/cc.js
@@ -0,0 +1,50 @@
+﻿/**
+ * Created by Mahboob.M on 5/2/16.
+ */
+
+CC = function (data, options, indicators) {
+
+    IndicatorBase.call(this, data, options, indicators);
+    /* Coppock = WMA[10] of  (ROC[14] + ROC[11]).*/
+    this.shortRoc = new ROC(data, { period: options.shortRocPeriod, appliedTo: options.appliedTo }, indicators);
+    this.longRoc = new ROC(data, { period: options.longRocPeriod, appliedTo: options.appliedTo }, indicators);
+
+    rocData=[];
+    for (var index = 0; index < data.length; index++) {
+        rocData.push({ time: data[index].time, close: this.shortRoc.indicatorData[index].value + this.longRoc.indicatorData[index].value });
+    };
+
+    this.wmaData = new WMA(rocData, { period: options.wmaPeriod }, indicators);
+    this.indicatorData = this.wmaData.indicatorData;
+};
+
+CC.prototype = Object.create(IndicatorBase.prototype);
+CC.prototype.constructor = CC;
+
+CC.prototype.addPoint = function (data) {
+    console.log('Adding CC data point : ', data);
+    var shortRoc = this.shortRoc.addPoint(data)[0].value;
+    var longRoc = this.longRoc.addPoint(data)[0].value;
+    var cc = this.wmaData.addPoint({ time: data.time, close: shortRoc + longRoc })[0].value;
+    this.indicatorData = this.wmaData.indicatorData;
+    return [{
+        id: this.uniqueID,
+        value: cc
+    }];
+};
+
+CC.prototype.update = function (data) {
+    console.log('Updating CC data point : ', data);
+    var shortRoc = this.shortRoc.update(data)[0].value;
+    var longRoc = this.longRoc.update(data)[0].value;
+    var cc = this.wmaData.update({ time: data.time, close: shortRoc + longRoc })[0].value;
+    this.indicatorData = this.wmaData.indicatorData;
+    return [{
+        id: this.uniqueID,
+        value: cc
+    }];
+};
+
+CC.prototype.toString = function () {
+    return 'CC (' + this.options.shortRocPeriod + ', ' + this.options.longRocPeriod + ', ' + this.options.wmaPeriod + ', ' + this.indicators.appliedPriceString(this.options.appliedTo) + ')';
+};
