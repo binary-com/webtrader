205d6656d83986a4cc287d31fc82936bbf8028f0
Arnab Karmakar <arnab@binary.com>
2016-04-16 23:10:55 -0600

    Fix min/max border conditions for indicator levels

--- src/charts/indicators/level.js
+++ src/charts/indicators/level.js
@@ -80,7 +80,7 @@ define(["jquery", "jquery-ui", 'color-picker', 'ddslick'], function ($) {
                                 val = Math.round(_.toNumber($elem.val()) * 10000)/10000;   //Maximum upto 4 decimals rounding
                                                     
 
-                            if (!_.isFinite(val) || !_.inRange(val, _.toNumber($elem.attr("min")), _.toNumber($elem.attr("max")) + 1)) {
+                            if (!_.isFinite(val) || !(val >=$elem.attr("min") && val <=$elem.attr("max"))) {
                                 require(["jquery", "jquery-growl"], function($) {
                                     $.growl.error({
                                         message: "Only numbers between " + $elem.attr("min") + " to " + $elem.attr("max") + " is allowed for " + $elem.closest('tr').find('td:first').text() + "!"
