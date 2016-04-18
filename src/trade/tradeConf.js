1db0193a1a5156e0683b1294116dde84d53242ad
Arnab Karmakar <arnab@binary.com>
2016-04-17 22:31:04 -0600

    Fix tick trade issue

--- src/trade/tradeConf.js
+++ src/trade/tradeConf.js
@@ -108,15 +108,15 @@ define(['lodash', 'jquery', 'moment', 'websockets/binary_websockets', 'common/ri
           if(state.ticks.category !== 'Digits')
               state.ticks.update_status();
       }
-      var key = chartingRequestMap.keyFor(symbol, 0);
-      var ticks  = barsTable.find({instrumentCdAndTp: key});
-      var start_time = state.buy.start_time*1000;
-      ticks = ticks.filter(function(tick) { return tick.time > start_time; })
-                   .map(function(tick) { return {quote: tick.price, epoch: tick.time/1000 }; })
-                   .sort(function(a,b) { return a.epoch - b.epoch; });
-      // console.warn(ticks);
       /* Wait till the view is bound via rivetsjs */
         _.defer(function(){
+            var key = chartingRequestMap.keyFor(symbol, 0);
+            var ticks  = barsTable.find({instrumentCdAndTp: key});
+            var start_time = state.buy.start_time*1000;
+            ticks = ticks.filter(function(tick) { return tick.time > start_time; })
+                .map(function(tick) { return {quote: tick.price, epoch: tick.time/1000 }; })
+                .sort(function(a,b) { return a.epoch - b.epoch; });
+            // console.warn(ticks);
             ticks.forEach(add_tick); /* add existing ticks */
             fn = liveapi.events.on('tick', function (data) {
                 console.log('tick trading, got ', data);
