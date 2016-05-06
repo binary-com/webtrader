6c5a952c84f314695d14cf12fabe6a0fa3ac8137
Apoorv Joshi <apoorv@binary.com>
2016-05-06 07:07:03 +0530

    Moved files to unsupported_browsers/

--- src/main.js
+++ src/main.js
@@ -85,7 +85,7 @@ require(["jquery", "modernizr", "common/util"], function( $ ) {
 
     //TODO if SVG, websockets are not allowed, then redirect to unsupported_browsers.html
     if (!Modernizr.svg || !Modernizr.websockets || Modernizr.touch) {
-      window.location.href = 'unsupported_browsers.html';
+      window.location.href = 'unsupported_browsers/unsupported_browsers.html';
       return;
     }
 
