7d9972d20951d649d3a9c64cb5ec0294ea3e7194
amin roosta <amin.roosta@outlook.com>
2016-05-14 15:08:30 +0430

    finish config dialog

--- src/config/config.js
+++ src/config/config.js
@@ -1,51 +1,82 @@
 /**
 * Created by arnab on 5/12/16.
 */
-define(['jquery', 'windows/windows', 'lodash', 'common/util'], function($, windows, _) {
+define(['jquery', 'windows/windows', 'common/rivetsExtra', 'lodash', 'common/util'], function($, windows, rv, _) {
   require(['text!config/config.html']);
   require(['css!config/config.css']);
 
-  var win = null, confirm = null;
-
-  function apply() {  }
-  function reset() { }
+  var win = null, win_view = null;
 
   function initConfigWindow(root) {
     root = $(root);
+    var state = init_state(root);
+    win_view = rv.bind(root[0], state);
 
     win = windows.createBlankWindow(root, {
-      title: 'Change Backend Server',
+      title: 'Configurations',
       resizable: false,
       collapsable: false,
       minimizable: false,
       maximizable: false,
       modal: true,
       ignoreTileAction:true,
-      'data-authorized': 'true',
       open: function () {
-        var storedWsURL = local_storage.get('websocket_url');
-        var api_url = (storedWsURL && storedWsURL.url)  || 'wss://ws.binaryws.com/websockets/v3?l=EN';
-        root.find('input').val(api_url);
+        var config = local_storage.get('config');
+        if(config && config.app_id) state.app_id = config.app_id;
+        if(config && config.server_url) state.server_url = config.server_url;
       },
       close: function() {
+        win_view && win_view.unbind();
         win && win.dialog('destroy').remove();
-        win = null;
+        win_view = win = null;
       },
       buttons: [
         {
           text: 'Apply',
           icons: { primary: 'ui-icon-check' },
-          click: apply
+          click: state.apply
         },
         {
           text: 'Reset default',
           icons: { primary: 'ui-icon-refresh' },
-          click: reset
+          click: state.reset
         }
       ]
    });
 
     win.dialog( 'open' );
+  }
+
+  function init_state(root) {
+    var state = {
+      server_url: 'ws.binaryws.com',
+      app_id: ''
+    };
+
+    state.apply = function() {
+      var config = {
+        server_url: state.server_url,
+        websocket_url: 'wss://' + state.server_url + '/websockets/v3?l=EN',
+        oauth_url: 'https://' + state.server_url + '/oauth2/authorize',
+        app_id: state.app_id
+      }
+      local_storage.set('config', config);
+      state.reload_page();
+    }
+
+    state.reset = function(){
+      local_storage.remove('config');
+      state.reload_page();
+    }
+
+    state.reload_page = function() {
+      $.growl.notice({message: 'Config changes successfull.<br/>Reloading page ...'});
+      setTimeout(function(){
+        window.location.reload();
+      }, 900);
+    }
+
+    return state;
   }
 
 
