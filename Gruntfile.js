e6e9fa8ca50a0149c4123f92411948c9818ca7f7
ashkanx <ashkan.nemati@gmail.com>
2016-11-18 11:15:53 +0800

    edit grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -235,7 +235,7 @@ module.exports = function (grunt) {
                 options: {
                     base: 'dist/compressed',
                     add: true,
-                    repo: 'https://' + process.env.GIT_KEY + '@github.com/binary-com/webtrader.git',
+                    repo: 'https://' + process.env.GIT_KEY + '@github.com/champion-fx/webtrader.git',
                     message: 'Commiting v<%=pkg.version%> using TravisCI and GruntJS build process'
                 },
                 src: ['**/*']
