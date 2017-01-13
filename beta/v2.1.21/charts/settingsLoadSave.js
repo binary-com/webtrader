cdbbad120bce907fbdad9e2569ce64b0e14f0d25
Arnab Karmakar <arnab@binary.com>
2017-01-13 06:59:57 +0000

    Commit v2.1.21 from TravisCI for [development]

---
+++ beta/v2.1.21/charts/settingsLoadSave.js
@@ -0,0 +1,1 @@
+define(["websockets/binary_websockets","windows/windows","common/rivetsExtra"],function(){require(["text!charts/settingsLoadSave.html"]),require(["css!charts/settingsLoadSave.css"])});
