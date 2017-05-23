a8ba6fe6f4e92cfb72c91a7460620d86e8765429
Apoorv Joshi <apoorv@binary.com>
2017-05-23 13:06:16 +0800

    Tests for touch/no-touch

---
+++ tests/touchNoTouchTradeTest.js
@@ -0,0 +1,36 @@
+const changeTradeType = (browser) => {
+  browser
+    .click('.trade-dialog .trade-fields .categories-row li:first-of-type .ui-selectmenu-button')
+    .click('.ui-selectmenu-menu.ui-selectmenu-open > ul > li:nth-of-type(2)')
+    .assert.containsText('.trade-dialog .trade-fields .categories-row li:first-of-type .ui-selectmenu-button .ui-selectmenu-text', 'Touch/No Touch')
+};
+
+export default {
+  touch: (browser) => {
+    changeTradeType(browser);
+    browser
+      .waitForCSSProperty('.trade-dialog .trade-fields .purchase-row', 'opacity', '1')
+      .assert.containsText('.trade-dialog .trade-fields .contract-displays .active', 'Touch')
+      .execute(() => { $('.trade-dialog .trade-fields .purchase-row > li > button').click() })
+      .waitForElementPresent('.trade-dialog .trade-conf')
+      .assert.containsText('.trade-dialog .trade-conf .title-row > li > span', 'Contract Confirmation')
+      .waitForElementVisible('.trade-dialog .trade-conf .close')
+      .execute(() => { $(".trade-dialog .trade-conf .close").click() })
+      .assert.elementNotPresent('.trade-dialog .trade-conf')
+  },
+  noTouch: (browser) => {
+    //Can be removed!
+    changeTradeType(browser);
+    browser
+      .click('.trade-dialog .trade-fields .contract-displays [data-name="no touch"]')
+      .waitForCSSProperty('.trade-dialog .trade-fields .purchase-row', 'opacity', '1')
+      .assert.containsText('.trade-dialog .trade-fields .contract-displays .active', 'No touch')
+      .execute(() => { $('.trade-dialog .trade-fields .purchase-row > li > button').click() })
+      .waitForElementPresent('.trade-dialog .trade-conf')
+      .assert.containsText('.trade-dialog .trade-conf .title-row > li > span', 'Contract Confirmation')
+      .waitForElementVisible('.trade-dialog .trade-conf .close')
+      .execute(() => { $(".trade-dialog .trade-conf .close").click() })
+      .assert.elementNotPresent('.trade-dialog .trade-conf')
+      
+  }
+}
--- tests/trade.test.js
+++ tests/trade.test.js
@@ -2,7 +2,9 @@
  * Tests for trade dialog
  */
 import { beforeLogin, after } from './default';
-
+import * as UpDownTests from './upDownTradeTest';
+import * as TouchNoTouchTests from './touchNoTouchTradeTest';
+import { tradeTemplate } from './tradeTemplateTest';
 module.exports = {
   before: (browser) => {
     beforeLogin(browser);
@@ -18,66 +20,10 @@ module.exports = {
       .waitForElementPresent('.trade-dialog')
   },
   after: after,
-  'Up/Down (RISE)': (browser) => {
-    browser
-      //Check that RISE is selected by default
-      .assert.cssProperty('.trade-dialog .trade-fields .contract-displays [data-name="rise"]', 'font-weight', 'bold')
-      .waitForElementPresent('.trade-dialog .trade-fields .date-start-row')
-      .assert.hidden('.trade-dialog .trade-fields .duration-row > li:last-of-type')
-      .click('.trade-dialog .trade-fields .duration-row > li:first-of-type > span')
-      .click('div[role="dialog"] .ui-selectmenu-open > ul > li:last-of-type')
-      // Check if End time options are visible
-      .assert.visible('.trade-dialog .trade-fields .duration-row li[rv-show="duration.value | eq \'End Time\' "]')
-      //Switch back to duration
-      .click('.trade-dialog .trade-fields .duration-row > li:first-of-type > span')
-      .click('div[role="dialog"] .ui-selectmenu-open > ul > li:first-of-type')
-  },
-  'Up/Down (FALL)': (browser) => {
-    browser
-      .click('.trade-dialog .trade-fields .contract-displays [data-name="fall"]')
-      .assert.containsText('.trade-dialog .trade-fields .contract-displays > .active', 'Fall')
-  },
-  'Up/Down (HIGHER)': (browser) => {
-    browser
-      .click('.trade-dialog .trade-fields .contract-displays [data-name="higher"]')
-      .assert.containsText('.trade-dialog .trade-fields .contract-displays > .active', 'Higher')
-      .assert.visible('.trade-dialog .trade-fields .barriers-barrier-row')
-  },
-  'Up/Down (LOWER)': (browser) => {
-    browser
-      .click('.trade-dialog .trade-fields .contract-displays [data-name="lower"]')
-      .assert.containsText('.trade-dialog .trade-fields .contract-displays > .active', 'Lower')
-      .assert.visible('.trade-dialog .trade-fields .barriers-barrier-row')
-  },
-  // This can be made optional.
-  'Tick trade': (browser) => {
-    browser
-      // Check if the purchase button is enabled
-      .waitForCSSProperty('.trade-dialog .trade-fields .purchase-row', 'opacity', '1')
-      // Purchase contract
-      .execute("$('.trade-dialog .trade-fields .purchase-row > li > button').click()")
-      .waitForElementPresent('.trade-dialog .trade-conf')
-      .waitForElementVisible('.trade-dialog .trade-conf .tick-chart')
-      /*
-      .waitForElementPresent('.trade-dialog .trade-conf .highcharts-container svg \
-        .highcharts-series-group .highcharts-markers .highcharts-point')*/
-      // Monitoring tick trade
-      .waitForSeriesData('.trade-dialog .trade-conf .tick-chart', 5)
-      .waitForElementVisible('.trade-dialog .trade-conf .close')
-      .execute('$(".trade-dialog .trade-conf .close").click()')
-      .assert.elementNotPresent('.trade-dialog .trade-conf')
-  },
-  'Trade template': (browser) => {
-    browser
-      .click('.trade-fields .categories-row .trade-template-manager > .img')
-      .assert.visible('.trade-fields .categories-row .trade-template-manager-root')
-      .click('.trade-fields .categories-row .trade-template-manager-root .button-secondary[rv-on-click="menu.save_as"]')
-      .assert.visible('.trade-fields .categories-row .trade-template-manager-root .save-as')
-      .setValue('.trade-fields .categories-row .trade-template-manager-root .save-as form > input', '(Some)')
-      .click('.trade-fields .categories-row .trade-template-manager-root .save-as form > button')
-      .assert.visible('.trade-fields .categories-row .trade-template-manager-root .menu')
-      .click('.trade-fields .categories-row .trade-template-manager-root .menu [rv-on-click="menu.templates"]')
-      .assert.visible('.trade-fields .categories-row .trade-template-manager-root .templates')
-      .assert.containsText('.trade-fields .categories-row .trade-template-manager-root .templates > div > .template .name', 'Up/Down Lower(Some)')
-  }
+  'Rise': UpDownTests.rise,
+  'Fall': UpDownTests.fall,
+  'Higher': UpDownTests.higher,
+  'Lower': UpDownTests.lower,
+  'Touch': TouchNoTouchTests.touch,
+  'Trade template': tradeTemplate
 }
