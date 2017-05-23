470d0f1935e8558c807c36bc67b5d4bfdefe4275
Apoorv Joshi <apoorv@binary.com>
2017-05-23 12:31:12 +0800

    Add tests for Up/Down Trade

--- tests/custom_commands/waitForSeriesData.js
+++ tests/custom_commands/waitForSeriesData.js
@@ -1,25 +1,30 @@
+const trade_results = {
+  'Up': (barrier, exit_spot) => barrier < exit_spot,
+  'Down': (barrier, exit_spot) => barrier > exit_spot
+}
+
 exports.command = function (selector, dataCount) {
   const timeout = this.globals.waitForConditionPollInterval; // Default Poll interval
   return this.perform((browser, done) => {
     let test_count = 0;
     let prev_length = 0;
     const check_property = () => {
-      browser.execute("return $('" +
-        selector.replace(/['"]/g, '\\\'') + "').highcharts().series[0].data.length",
+      browser.execute((selector) => {
+        const series = $(selector).highcharts().series[0];
+        const series_length = series.data.length;
+        const last_data = series_length && series.data[series_length - 1].y;
+        const barrier = series.yAxis.plotLinesAndBands.length && series.yAxis.plotLinesAndBands[0].options.value;
+        return [series_length, barrier, last_data];
+      }, [selector],
         (result) => {
-          if (result.value == dataCount + 1) {
-            browser.execute("return $('" +
-              selector.replace(/['"]/g, '\\\'') + "').highcharts().series[0].xAxis.plotLinesAndBands[1].label.textStr",
-              (result) => {
-                if (result.value === "Exit Spot") {
-                  console.log(' \x1b[1;32m✔\x1b[m Found Exit spot.');
-                  done();
-                } else {
-                  browser.assert.fail('Exit spot to be present.', 'Exit spot not found.',
-                    'Unexpected error.', '');
-                }
-              });
-          } else if (result.value > prev_length) {
+          if (result.value[0] == dataCount + 1) {
+            if (trade_results[browser.tradeDirection]) {
+              const isWin = trade_results[browser.tradeDirection](result.value[1], result.value[2]);
+              browser.assert.containsText('.trade-dialog .trade-conf .title-row > li > span',
+                isWin ? 'This contract won' : 'This contract lost');
+            }
+            done();
+          } else if (result.value[0] > prev_length) {
             test_count = 0;
             prev_length++;
             console.log(' \x1b[1;32m✔\x1b[m Got tick "' + prev_length + '".');
