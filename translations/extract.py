87233957c424ac12ebb251c0e584761ace22b2c5
amin roosta <amin.roosta@outlook.com>
2017-06-18 17:35:03 +0430

    add translations for webtrader-charts

--- translations/extract.py
+++ translations/extract.py
@@ -24,7 +24,29 @@ static = [
     '4 Hours', '8 Hours', '1 Day', 'Candles', 'OHLC', 'Line', 'Dot', 'Line Dot', 'Spline', 'Table', '1t', '1m', '2m', '3m', 
     '5m', '10m', '15m', '30m', '1h', '2h', '4h', '8h', '1d', '1', '2', '3', '4', '5', '8', '10', '15', '30', 
     'Add/remove indicators', 'Touch', 'No touch', 'Ends out', 'Ends in', 'Stays in', 'Goes out', 'Matches', 'Differs',
-    'Under', 'Odd', 'Even', 'Asian up', 'Asian down', 'Spreads up', 'Spreads down', 'Open', 'High', 'Low', 'Close'
+    'Under', 'Odd', 'Even', 'Asian up', 'Asian down', 'Spreads up', 'Spreads down', 'Open', 'High', 'Low', 'Close',
+
+    # from webtrader-charts
+   'Add comparisons', 'Current comparisons', 'Current indicators', 'Favorites', 'Add indicator', 'Volatility Indicators',
+   'Overlap Studies', 'Momentum Indicators', 'Price Transformation', 
+   'Statistical Functions', 'Pattern Recognition', 'Bill Williams',
+   'Time interval', 'Chart type', 'Indicators', 'Comparisons', 'Drawing tools',
+   'Chart template', 'Share chart', 'Share', 'Link', 'Embed code', 'Download as',
+   'Candles', 'OHLC', 'Line' , 'Dot' , 'Line Dot' , 'Spline' , 'Table', 'Crosshair',
+   'Vertical line', 'Horizontal line', 'Tick', '1 Tick', '1 Minute', '1 Hour',
+   '1 Day', 'Ticks', 'Minutes', 'Hours', 'Days',
+   'Cancel', 'OK', 'Delete', 'Remove', 'Rename', 'Back', 'Save', 'Yes', 'No',
+   'Load chart template', 'Save changes', 'Save as', 'Upload template',
+   "You haven't saved any templates yet", 'Name', 'This field is required',
+   'Use the "Save as ..." button to save the chart settings, type, time period, indicators and comparisons',
+   'Open', 'High', 'Low', 'Close',
+   '1t', '1m', '2m', '3m', '5m', '10m', '15m', '30m',
+   '1h', '2h', '4h', '8h', '1d', 'Invalid parameter(s)',
+   'Are you sure you want to delete template?', 'Are you sure you want to overwrite current template?', 
+   'Template changes saved', 'Invalid JSON file', 'Invalid template type', 'Successfully applied the template and saved it as',
+   'Downloading template as', 'Template name already exists',
+   'Add/Remove indicators', 'Add/Remove comparisons',
+   'Error getting data for %1', 'Downloading .csv', 'Stroke width', 'Stroke color'
 ]
 
 # Parsing webtrader html files.
