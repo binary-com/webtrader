520a44b5c9a06cff60f6340fa6ec051e4af43d86
OskarAhl <oskar.ahlroth@gmail.com>
2018-10-10 09:59:41 +0800

    update comment, remove log, add css variables for colors

--- translations/extract.py
+++ translations/extract.py
@@ -112,7 +112,6 @@ os.remove(messages_pot);
 with open(messages_pot, 'a+') as wf:
     for text in texts:
         text = text.replace('"', '\\"')
-        print text
         text = re.sub(r'[^\x00-\x7F]+',' ', text).strip() # remove non aski characters
         if text:
             wf.write('\n' + 'msgid "' + text + '"'      )
