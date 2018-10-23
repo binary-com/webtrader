2a4e3ec04a2887d8e6bc74f8080c9679255e3fbf
OskarAhl <oskar@binary.com>
2018-10-23 02:26:35 +0000

    Commit v2.2.8 from TravisCI for [development]

---
+++ beta/v2.2.8/lib/highstock-release/themes/avocado.js
@@ -0,0 +1,1 @@
+!function(a){"object"==typeof module&&module.exports?module.exports=a:a(Highcharts)}(function(a){a.theme={colors:["#F3E796","#95C471","#35729E","#251735"],colorAxis:{maxColor:"#05426E",minColor:"#F3E796"},plotOptions:{map:{nullColor:"#fcfefe"}},navigator:{maskFill:"rgba(170, 205, 170, 0.5)",series:{color:"#95C471",lineColor:"#35729E"}}},a.setOptions(a.theme)});
