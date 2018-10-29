8c443cce13ce89bd4c5b2d75bcc91073a15c5960
OskarAhl <oskar@binary.com>
2018-10-29 07:19:01 +0000

    Commit v2.2.8 from TravisCI for [master]

---
+++ v2.2.8/lib/highstock-release/themes/sunset.js
@@ -0,0 +1,1 @@
+!function(a){"object"==typeof module&&module.exports?module.exports=a:a(Highcharts)}(function(a){a.theme={colors:["#FDD089","#FF7F79","#A0446E","#251535"],colorAxis:{maxColor:"#60042E",minColor:"#FDD089"},plotOptions:{map:{nullColor:"#fefefc"}},navigator:{series:{color:"#FF7F79",lineColor:"#A0446E"}}},a.setOptions(a.theme)});
