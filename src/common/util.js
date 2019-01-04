2bb632a5627d2b2214a033f2ced2af78691e5445
Amam Mustofa <mustofa@binary.com>
2019-01-04 11:12:56 +0800

    revert isCrypto function

--- src/common/util.js
+++ src/common/util.js
@@ -10,11 +10,6 @@ function isLineDotType(type) {
     return type === "linedot";
 }
 
-function isCrypto() {
-    var crypto_currencies = ['BTC', 'LTC', 'ETH', 'BCH', 'ETC', 'DAI', 'UST'];
-    return crypto_currencies.includes(local_storage.get('authorize').currency);
-}
-
 function convertToTimeperiodObject(timePeriodInStringFormat) {
     return {
         intValue : function() {
