055fffd3f1891fbc3dc568422972cc0a832acf08
Amam Mustofa <mustofa@binary.com>
2019-01-24 00:37:49 +0800

    remove phone unsupported

--- src/main.js
+++ src/main.js
@@ -111,7 +111,7 @@ window.requirejs.onError = function(err) {
 
 require(["modernizr"], function() {
     var Modernizr = window.Modernizr;
-    if (!Modernizr.svg || !Modernizr.websockets || (Modernizr.touch && window.isSmallView()) || 
+    if (!Modernizr.svg || !Modernizr.websockets || 
         !Modernizr.localstorage || !Modernizr.webworkers || !Object.defineProperty) {
         window.location.assign("unsupported_browsers/unsupported_browsers.html");
         return;
