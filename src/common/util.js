ddaa0f1bffe6c75b813c018d4600072991d05379
Amam Mustofa <mustofa@binary.com>
2019-06-15 03:31:35 +0800

    fix missing exit spot and quote

--- src/common/util.js
+++ src/common/util.js
@@ -131,6 +131,17 @@ function addComma(num, decimal_points, is_crypto) {
     });
 };
 
+function getSymbolPipValue(symbol) {
+  var active_symbols = local_storage.get('active_symbols');
+  var selected_symbol = active_symbols.find(function(item) {
+    return item.symbol === symbol;
+  });
+  var symbol_pip = selected_symbol.pip.toString();
+  var pip_value = symbol_pip.substring(symbol_pip.indexOf(".") + 1).length || 4;
+
+  return pip_value;
+};
+
 function sortAlphaNum(property) {
     "use strict";
     var reA = /[^a-zA-Z]/g;
