be39545c2ec32aa1635d496e93db074886df7661
ci-build <ci-build@binary.com>
2020-07-01 03:32:47 +0000

    [skip ci]

---
+++ v2.2.8/lib/highstock-release/themes/sunset.js
@@ -0,0 +1,1 @@
+!function(o){"object"==typeof module&&module.exports?module.exports=o:o(Highcharts)}(function(o){o.theme={colors:["#FDD089","#FF7F79","#A0446E","#251535"],colorAxis:{maxColor:"#60042E",minColor:"#FDD089"},plotOptions:{map:{nullColor:"#fefefc"}},navigator:{series:{color:"#FF7F79",lineColor:"#A0446E"}}},o.setOptions(o.theme)});
