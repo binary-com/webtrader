25d087d502ef7998ed093b58751a6f35e131fb10
ci-build <ci-build@binary.com>
2020-07-01 01:56:53 +0000

    [skip ci]

---
+++ v2.2.8/common/InContextTranslation.js
@@ -0,0 +1,1 @@
+var _jipt=[];if("ach"===(local_storage.get("i18n")||{}).value){localStorage.setItem("jipt_language_code_webtrader","en"),_jipt.push(["project","webtrader"]);var crowdin=document.createElement("script");crowdin.setAttribute("src","//cdn.crowdin.com/jipt/jipt.js"),document.head.appendChild(crowdin)}
---
+++ v2.2.8/lib/highstock-release/themes/sunset.src.js
@@ -0,0 +1,1 @@
+"use strict";!function(o){"object"==typeof module&&module.exports?module.exports=o:o(Highcharts)}(function(o){var e;(e=o).theme={colors:["#FDD089","#FF7F79","#A0446E","#251535"],colorAxis:{maxColor:"#60042E",minColor:"#FDD089"},plotOptions:{map:{nullColor:"#fefefc"}},navigator:{series:{color:"#FF7F79",lineColor:"#A0446E"}}},e.setOptions(e.theme)});
