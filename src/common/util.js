62d572b5a186b54651b05925f49c50a95b4e0bfd
mahdiyeh-fs <mahdiyeh@firstsource.tech>
2021-07-19 10:18:40 +0430

    remove symbol filtering in webtrader

--- src/common/util.js
+++ src/common/util.js
@@ -1,5 +1,5 @@
 function filterRestrictedSymbols(active_symbols) {
-  return active_symbols.filter(function(item) { return !/^(BOOM|CRASH|STP).+/i.test(item.symbol)});
+  return active_symbols;
 }
 
 function isTick(ohlc) {
